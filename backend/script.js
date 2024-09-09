6afc303753c0af96dd26c3734d7c2effadfe0896
TDo204 <156011454+TDo204@users.noreply.github.com>
2024-09-09 19:51:49 -0400

    Added functions to work on for script.js

---
+++ backend/script.js
@@ -0,0 +1,34 @@
+const urlBase = 'http://mattct027.xyz/API';
+const extension = 'php';
+
+let userID = 0;
+let firstName = "";
+let lastName = "";
+
+function signup() {
+
+}
+
+function login() {
+
+}
+
+function logout() {
+
+}
+
+function createContact() {
+
+}
+
+function retrieveContact() {
+    
+}
+
+function updateContact() {
+    
+}
+
+function deleteContact() {
+    
+}
