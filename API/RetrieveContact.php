110dd1e0520fe87f81a47333c2a64b31ab9beefa
JoaoMarcelo <joaomarsil4@gmail.com>
2024-09-11 10:21:38 -0400

    Updated RetrieveContact

---
+++ API/RetrieveContact.php
@@ -0,0 +1,74 @@
+<?php 
+    
+    // Get input data
+    $inData = getRequestInfo();
+
+    // Setting the variables
+    $searchResults = "";
+    $searchCount = 0;
+
+    // Connect to the database
+    $conn = new mysqli("localhost", "MasterUser", "smallproject", "SmallProject");
+
+    // Check if connection was successful
+    if($conn->connect_error){
+        returnWithError($conn->connect_error); // Failed connection
+    } else {
+        // Search the contact 
+        $stmt = $conn->prepare("SELECT name FROM Contacts WHERE name LIKE ? AND user_ID = ?");
+
+        // Search pattern 
+        $contactName = "%" . $inData["search"] . "%";
+        $stmt->bind_param("ss", $contactName, $inData["user_ID"]);
+
+        // Execute SQL
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        // Finding the user/users
+        while($row = $result->fetch_assoc()){
+            if($searchCount > 0){
+                $searchResults .= ",";
+            }
+            $searchCount++;
+            $searchResults .='"' . $row["name"] . '"';
+        }
+
+        // User was not found
+        if($searchCount == 0){
+            returnWithError("No Records Found");
+        } else {
+            returnWithInfo($searchResults);
+        }
+
+        // Close connections
+        $stmt->close();
+        $conn->close();
+        
+    }
+
+    // Helper Functions
+    function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+	function returnWithInfo( $searchResults )
+	{
+		$retValue = '{"results":[' . $searchResults . '],"error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
