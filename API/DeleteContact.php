0b25d1e94ccf20eb22999328527e17be9c074b05
JoaoMarcelo <joaomarsil4@gmail.com>
2024-09-11 10:40:05 -0400

    Comments were added

--- API/DeleteContact.php
+++ API/DeleteContact.php
@@ -3,23 +3,28 @@
     // Get input data
     $inData = getRequestData();
 
+    // Connect to the database
     $con = new mysqli("localhost", "MasterUser", "smallproject", "SmallProject");
 
+    // Check connection
     if($conn->connect_error){
-        returnWithError($conn->connect_error);
+        returnWithError($conn->connect_error); // Connection failed
     }
     else {
+        // Delete contact
         $stmt = $conn->prepare("DELETE FROM Contacts WHERE name = ? AND email = ? AND phone = ? AND user_ID = ?");
         $stmt->bind_param("sss", $inData["name"], $inData["email"], $inData["phone"], $inData["user_ID"]);
 
         $stmt->execute();
 
+        // Check if contact was delete or it does not exist
         if($stms->affected_rows > 0){
             sendResultInfoAsJson('{"message": "Contact deleted."}');
         } else {
             returnWithError("No contact found.");
         }
 
+        // Closing connections
         $stmt->close();
         $conn->close();
     }
--- API/Login.php
+++ API/Login.php
@@ -1,32 +1,41 @@
 <?php
-
+    
+    // Get Input Data
     $inData = getRequestInfo();
 
+    // Setting variables
     $id = 0;
     $firstName = "";
     $lastName = "";
 
+    // Connecting with the database 
     $conn = new mysqli("localhost", "MasterUser", "smallproject", "SmallProject");
 
+    // Check connection 
     if($conn -> connect_error){
-        returnWithError($conn->connect_error);
+        returnWithError($conn->connect_error); // Connection failed
     } else {
+        // Gets the user
         $stmt = $conn->prepare("SELECT ID, firstName, lastName FROM Users WHERE username = ? AND password = ?");
         $stmt->bind_param("ss", $inData["username"], $inData["password"]);
 
+        // Execution
         $stmt->execute();
         $result = $stmt->get_result();
 
+        // Return info if user is found or not found
         if($row = $result->fetch_assoc()){
             returnWithInfo($row['firstName'], $row['lastName'], $row['ID']);
         } else {
             returnWithError("Not found");
         }
 
+        // Closing Connections
         $stmt->close();
         $conn->close();
     }  
 
+    // Helper Functions
     function getRequestInfo(){
         return json_decode(file_get_contents('php://input'), true);
     }
