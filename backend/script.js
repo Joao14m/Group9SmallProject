524af013eccf1e1fb324a740e7febf74bbcf1987
JoaoMarcelo <joaomarsil4@gmail.com>
2024-09-11 18:08:52 -0400

    Login and Logout Functions Updated

--- backend/script.js
+++ backend/script.js
@@ -10,11 +10,53 @@ function signup() {
 }
 
 function login() {
+    userId = 0;
+    firstName = "";
+    lastName = "";
 
+    let login = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+
+    // document.getElementById("loginResult").innerHTML = "";
+
+    let tmp = {login:login, password:password};
+    let jsonPayload = JSON.stringify(tmp);
+
+    let url = urlBase + '/Login.' + extension;
+
+    let xhr = new HMLHttRequest();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+    
+    try{
+        xhr.onreadystatechange = function(){
+            if(this.readyState = 4 && this.status == 200){
+                let jsonObject = JSON.parse(xhr.responseText);
+                userId = jsonObject.id;
+
+                if(userId < 1){
+                    return;
+                }
+
+                firstName = jsonObject.firstName;
+                lastName = jsonObject.lastName;
+
+                window.location.href = "index.html";
+            }
+        };
+        xhr.send(jsonPayload);
+    }
+
+    catch(err){
+        document.getElementById("loginResult").innerHTML = err.message;
+    }
 }
 
 function logout() {
-
+    userId = 0;
+    firstName = "";
+    lastName = "";
+    window.location.href = "index.html";
 }
 
 function createContact() {
