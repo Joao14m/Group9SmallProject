c206e754382461d56ef55257c8f9a317c4ce9f7d
TDo204 <156011454+TDo204@users.noreply.github.com>
2024-09-11 19:12:53 -0400

    Added signup function to backend

--- backend/script.js
+++ backend/script.js
@@ -1,12 +1,48 @@
 const urlBase = 'http://mattct027.xyz/API';
 const extension = 'php';
 
-let userID = 0;
+let userId = 0;
 let firstName = "";
 let lastName = "";
 
 function signup() {
+    userId = 0;
+    firstName = document.getElementById("firstName").value;
+    lastName = document.getElementById("lastName").value;
 
+    let login = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+
+    // document.getElementById("signupResult").innerHTML = "";
+
+    let tmp = {firstName:firstName, lastName:lastName, login:login, password:password};
+    let jsonPayload = JSON.stringify(tmp);
+
+    let url = urlBase + '/Signup.' + extension;
+    
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+
+    try{
+        xhr.onreadystatechange = function() {
+            if(this.readyState = 4 && this.status == 200){
+                let jsonObject = JSON.parse(xhr.responseText);
+                userId = jsonObject.id;
+
+                if(userId < 1){
+                    return;
+                }
+            
+                window.location.href = "index.html";
+            }
+        };
+        xhr.send(jsonPayload);
+    }
+
+    catch(err){
+        document.getElementById("signupResult").innerHTML = err.message;
+    }
 }
 
 function login() {
@@ -24,7 +60,7 @@ function login() {
 
     let url = urlBase + '/Login.' + extension;
 
-    let xhr = new HMLHttRequest();
+    let xhr = new XMLHttpRequest();
     xhr.open("POST", url, true);
     xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
     
